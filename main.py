f1a1995ef08956d3bcaa5f27a05a70c0e587e897
Artur Gomes <68240859+arturgomesc@users.noreply.github.com>
2023-12-15 20:26:21 -0300

    update main.py: changing txt file and add coments

--- main.py
+++ main.py
@@ -4,31 +4,27 @@ from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.pgvector import PGVector
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv() # carregar a API KEY da OpenAI 
 
-loader = TextLoader('thomas_sankara_speech.txt', encoding='utf-8')
+loader = TextLoader('universal_declaration_of_human_rights.txt', encoding='utf-8')
 documents = loader.load()
 
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=20)
-texts = text_splitter.split_documents(documents)
+texts = text_splitter.split_documents(documents) # Separar o documento em partes
 
 embeddings = OpenAIEmbeddings()
 
-vector = embeddings.embed_query('Testing the embedding model')
-
-doc_vectors = embeddings.embed_documents([t.page_content for t in texts[:5]])
-
-CONNECTION_STRING = "postgresql+psycopg2://postgres:7834@localhost:5432/vector_db2"
-COLLECTION_NAME = 'thomas_sankara_speech_vectors'
+CONNECTION_STRING = "postgresql+psycopg2://postgres:7834@localhost:5432/vector_db2" # conexão com o Postgresql
+COLLECTION_NAME = 'universal_declaration_of_human_rights_vectors'
 
 db = PGVector.from_documents(
     embedding=embeddings,
     documents=texts,
     collection_name=COLLECTION_NAME,
     connection_string=CONNECTION_STRING,
-)
+) # convertendo as partes do documento em embeddings e adicionando ao Postgresql com PGVector
 
-query = "What did Thomas Sankara say about justice"
+query = "What did ONU said about justice"
 
 similar = db.similarity_search_with_score(query, k=2)
 
